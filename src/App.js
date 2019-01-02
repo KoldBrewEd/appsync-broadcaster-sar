3d7c780343d1e85239aa298669cfec5a9bc6c799
Ed Lima <edlima@amazon.com>
2019-01-02 14:13:54 +1100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Amplify, {API,graphqlOperation} from 'aws-amplify';
+Amplify.configure({
+  'aws_appsync_graphqlEndpoint': 'https://xxxxxx.appsync-api.us-east-1.amazonaws.com/graphql',
+  'aws_appsync_region': 'us-east-1',
+  'aws_appsync_authenticationType': 'API_KEY',
+  'aws_appsync_apiKey': 'da2-xxxxxxxxxxxxxxxxxxxxxxxxxx',
+});
+
+const createMessage = `mutation createMessage($message: String!){
+  createMessage(input:{message:$message}) {
+    id
+    message
+  }
+}
+`;
+
+const onCreateMessage = `subscription onCreateMessage {
+  onCreateMessage {
+    __typename
+    message
+  }
+}`;
+
+class App extends Component {
+
+  constructor(props){
+     super(props);
+     this.state={
+        message:"",
+        value:"",
+        display:false
+     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async componentDidMount(){
+    this.subscription = API.graphql(graphqlOperation(onCreateMessage)).subscribe({
+      next: (event) => {
+          console.log("Subscription: "+event.value.data);
+          this.setState({display: true});
+          this.setState({message: event.value.data.onCreateMessage.message});
+        }
+    });
+  }
+
+  handleChange(event) {
+    this.setState({value:event.target.value});
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const message = {
+      "message":this.state.value
+    }
+    await API.graphql(graphqlOperation(createMessage, message));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="jumbotron jumbotron-fluid p-0">
+          <h3 className="display-4">Broadcaster</h3>
+        </div>
+        <br/>
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input className="form-control form-control-lg"type="text" value={this.state.value} onChange={this.handleChange} />
+          </div>
+          <input type="submit" value="Submit" className="btn btn-primary"/>
+          </form>
+        </div>
+        <br/>
+        {this.state.display ?  
+          <div className="container">
+            <div className="card bg-success">
+              <h3 className="card-text text-white p-2">{this.state.message}</h3>
+            </div>
+          </div>
+        : null }
+      </div>
+    );
+  }
+}
+
+export default App;
